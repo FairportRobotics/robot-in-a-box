f14bd1f6fafd7a82ecc1437521080ce71b4cff0a
Mike Silva <mike.a.silva@gmail.com>
2026-02-07 16:05:00 -0500

    More WIP

--- robot.py
+++ robot.py
@@ -1,10 +1,13 @@
 import wpilib
 import phoenix5
+from subsystems.hopper import HopperSubsystem
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.motor = phoenix5.WPI_TalonSRX(17)
+        #self.motor = phoenix5.WPI_TalonSRX(17)
+        self.hopper = HopperSubsystem(phoenix5.WPI_TalonSRX(17))
         
 
     def teleopPeriodic(self):
-        self.motor.set(0.5)
+        #self.motor.set(0.5)
+        pass
