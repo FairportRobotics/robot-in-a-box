4ef60e104143ad375f24d09a9c4b15523c7bd57a
rache143 <rachael.case@fairportrobotics.org>
2026-02-07 10:25:39 -0500

    your mom

--- robot.py
+++ robot.py
@@ -1,1 +1,12 @@
-# TODO: insert robot code here
+import wpilib 
+import phoenix5
+class Robot (wpilib.TimedRobot): 
+    import wpilib
+import phoenix5
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        self.motor = phoenix5.MotorController(1)
+
+    def teleopPeriodic(self):
+        self.motor.set(0.5)
