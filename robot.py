aa1c4a92300bd28add5c799d312789e51c1e1bdc
rache143 <rachael.case@fairportrobotics.org>
2026-02-07 12:51:47 -0500

    heh

--- robot.py
+++ robot.py
@@ -3,7 +3,7 @@ import phoenix5
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.motor = phoenix5.TalonSRX(17)
+        self.motor = phoenix5.WPI_TalonSRX(17)
 
     def teleopPeriodic(self):
         self.motor.set(0.5)
