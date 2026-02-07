c4c21e18a6bc020d10a876100c5da780f6acef15
rache143 <rachael.case@fairportrobotics.org>
2026-02-07 13:22:50 -0500

    base but idk what to do

--- robot.py
+++ robot.py
@@ -7,3 +7,25 @@ class MyRobot(wpilib.TimedRobot):
 
     def teleopPeriodic(self):
         self.motor.set(0.5)
+        
+class XboxController:
+    port: int
+
+    def execute(self):
+        pass
+
+    def setup(self):
+        self.controller = wpilib.XboxController(self.port)
+
+    def joysticks(self):
+        return (
+            self.controller.getLeftX(),
+            self.controller.getLeftY(),
+            self.controller.getRightX(),
+            self.controller.getRightY(),
+        )
+    def x_pressed(self):
+        return self.controller.getXButton()
+    
+    def y_pressed(self):
+        return self.controller.getYButton()
