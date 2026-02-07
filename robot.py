05c4e74522590464f423c5d2727584d5e8c53a0c
rache143 <rachael.case@fairportrobotics.org>
2026-02-07 10:30:48 -0500

    i wanna swing from the chandeliehee

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ import wpilib
 import phoenix5
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.motor = phoenix5.MotorController(1)
+        self.motor = phoenix5.TalonSRX(1)
 
     def teleopPeriodic(self):
         self.motor.set(0.5)
