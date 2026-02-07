4b38f71e166c873f5e013004245e19cd0a6fbf96
rache143 <rachael.case@fairportrobotics.org>
2026-02-07 14:07:08 -0500

    did the thing

--- robot.py
+++ robot.py
@@ -4,28 +4,9 @@ import phoenix5
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         self.motor = phoenix5.WPI_TalonSRX(17)
+        self.controller = wpilib.XboxController(0)
 
     def teleopPeriodic(self):
-        self.motor.set(0.5)
-        
-class XboxController:
-    port: int
-
-    def execute(self):
-        pass
-
-    def setup(self):
-        self.controller = wpilib.XboxController(self.port)
-
-    def joysticks(self):
-        return (
-            self.controller.getLeftX(),
-            self.controller.getLeftY(),
-            self.controller.getRightX(),
-            self.controller.getRightY(),
-        )
-    def x_pressed(self):
-        return self.controller.getXButton()
+        speed = self.controller.getLeftY()
+        self.motor.set(speed)
     
-    def y_pressed(self):
-        return self.controller.getYButton()
